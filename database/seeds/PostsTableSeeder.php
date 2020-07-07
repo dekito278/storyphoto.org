5d3c16d3cc96f3d2c74a56725921401574b0dc3a
dekito278 <aderizki68@gmail.com>
2020-07-07 00:21:01 -0700

    remove temporary artisan

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -11,11 +11,6 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Models\Post::class, 20)->create()->each(function ($post) {
-            App\Models\PostImage::create([
-                'post_id' => $post->id,
-                'post_image_path' => 'https://images.pexels.com/photos/302743/pexels-photo-302743.jpeg?cs=srgb&dl=ball-ball-shaped-blur-bubble-302743.jpg'
-            ]);
-        });
+       //
     }
 }
